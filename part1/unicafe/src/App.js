b6f7ba86a995faa02cba474a122f87879f6eaadc
Lorenzo Gravina <lorenzo.gravina@me.com>
2021-11-24 16:09:27 +0300

    complete exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+
+  let avg = (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+  let percentPositive = (good / (good + neutral + bad)) * 100
+
+  if (good + neutral + bad === 0) {
+    return (
+      <div>
+        No feedback given.
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}<br />
+        all {good + neutral + bad}<br />
+        average {avg}<br />
+        positive {percentPositive}%<br />
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let avg = (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
-  let percentPositive = (good / (good + neutral + bad)) * 100
 
   return (
     <div>
@@ -16,14 +40,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button onClick={() => setBad(bad + 1)} text="Bad" />
       <h1>Statistics</h1>
-      <div>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {good + neutral + bad}<br />
-        average {Number.isNaN(avg) ? 0 : avg}<br />
-        positive {percentPositive}%<br />
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
