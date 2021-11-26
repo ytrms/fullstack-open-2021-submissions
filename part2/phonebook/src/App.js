a2fa151067f91617528ca90838ea9d59c00c208a
ytrms <lorenzo.gravina@me.com>
2021-11-26 18:56:44 +0300

    complete exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -46,36 +46,68 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input
-          value={newFilter}
-          onChange={handleFilterChange} />
-      </div>
+      <Filter
+        value={newFilter}
+        handleFilterChange={handleFilterChange}
+      />
       <h2>Add a new person:</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      {personsToShow.map(person => {
-        return (
-          <div key={person.name}>{person.name} {person.number}</div>
-        )
-      })}
+      <Persons personsToShow={personsToShow} />
     </div>
+  )
+}
+
+const Filter = ({ value, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input
+        value={value}
+        onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  const { addPerson, newName, handleNameChange, newNumber, handleNumberChange } = props
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = ({ name, number }) => (
+  <div key={name}>{name} {number}</div>
+)
+
+const Persons = ({ personsToShow }) => {
+  return (
+    personsToShow.map(person => {
+      return (
+        <Person name={person.name} number={person.number} />
+      )
+    })
   )
 }
 
