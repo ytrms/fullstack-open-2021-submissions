67266abdda198b69ad03128a694468e578148221
ytrms <lorenzo.gravina@me.com>
2021-11-27 10:34:25 +0300

    complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPersonObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1
     }
 
     if (persons.find(person => person.name === newName)) {
@@ -97,15 +104,15 @@ const PersonForm = (props) => {
   )
 }
 
-const Person = ({ name, number }) => (
-  <div key={name}>{name} {number}</div>
+const Person = ({ name, number, id }) => (
+  <div key={id}>{name} {number}</div>
 )
 
 const Persons = ({ personsToShow }) => {
   return (
     personsToShow.map(person => {
       return (
-        <Person name={person.name} number={person.number} />
+        <Person name={person.name} number={person.number} key={person.id} />
       )
     })
   )
