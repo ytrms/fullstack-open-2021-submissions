7b218886ddce8568145c457e3ab305b02a280881
ytrms <lorenzo.gravina@me.com>
2021-11-27 11:40:11 +0300

    complete exercise 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+
+const CountryList = ({ countries }) => {
+  if (countries.length > 10) {
+    return <div>Too many results. Please narrow your search.</div>
+  }
+
+  if (countries.length === 1) {
+    const country = countries[0]
+    const languages = []
+    for (const key in country.languages) {
+      const language = country.languages[key];
+      languages.push({ key: key, language: language })
+    }
+
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <div>capital: {country.capital[0]}</div>
+        <div>population: {country.population}</div>
+        <h2>Languages</h2>
+        <ul>
+          {languages.map(language => {
+            return (
+              <li key={language.key}>{language.language}</li>
+            )
+          })}
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200px" />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {countries.map(country => {
+        return (
+          <div key={country.cca2}>{country.name.common}</div>
+        )
+      })}
+    </div>
+  )
+}
+
+function App() {
+  const [newSearch, setNewSearch] = useState("")
+  const [countryList, setCountryList] = useState([])
+
+  const hook = () => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(response => {
+        setCountryList(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  const countryListToShow = newSearch === ""
+    ? []
+    : countryList.filter(country => country.name.common.toLowerCase().includes(newSearch.toLowerCase()))
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      find countries <input
+        value={newSearch}
+        onChange={handleSearchChange}
+      />
+      <CountryList countries={countryListToShow} />
+    </div>
+  );
+}
+
+export default App;
